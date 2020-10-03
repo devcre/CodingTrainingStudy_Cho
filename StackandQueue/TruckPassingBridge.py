79e6bf0201f6792616ae05e347070e16cac18047
devcre <tigertank707@gmail.com>
2020-10-03 13:39:19 +0900

    add comments

--- StackandQueue/TruckPassingBridge.py
+++ StackandQueue/TruckPassingBridge.py
@@ -32,18 +32,25 @@ def solution(bridge_length, weight, truck_weights):
     tik = 0
     while True:
         tik += 1
+        # 다리를 건너는 트럭이 없고 대기 중인 트럭도 없으면 끝
         if len(t_w) == 0 and len(tob) == 0:
             break
         
+        # 대기 중인 트럭은 없고 다리를 건너는 트럭이 있음
         if len(t_w) == 0:
             pass
+
+        # 대기 중인 트럭을 추가해도 weight에 미치지 않고
+        # 다리에 트럭이 건널 길이(공간)이 있으면
+        # 같이 건너도 된다.
         elif ctw + t_w[0] < weight and col < b_l:
             ctw += t_w[0]
             col += 1
             newTruck = Truck(t_w[0], 0)
             tob.append(newTruck)
             del t_w[0]
-                
+        
+        # 다리를 건너는 중인 트럭이 얼마나 이동했는지       
         for x in tob:
             x._time += 1
             if x._time == b_l:
